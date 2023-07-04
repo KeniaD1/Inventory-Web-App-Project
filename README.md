e2d0d4b8375800cbdcad836d70a9f3259586b1ed
keniaDE <decoteaukenia@gmail.com>
2023-07-04 17:51:22 -0400

    Readme file complete

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Inventory-Web-App-Project
+
+This web application will allow the user to view the inventory we have at the Bag Boutique . 
+
+On load user will see a few of the bags in stock. 
+
+User will have access to instock button that has toggle features allowing to change if item is out of stock .
+
+User will have to remove button that removes items from the page .
+
+User has access to a form will they can fill information based by :
+"Bag Name"
+"Bag Price"
+"Bag Image Url"
+"Instock"
+"Color"
+
+The form will submit a new item to the bag .
+
+The form has a reset button that reset the form on click. 
--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 
 
+const allRemoveButtons = document.querySelectorAll('#rmv');
+
+for (const rmvBttn of allRemoveButtons) {
+    rmvBttn.addEventListener('click', (event) => {
+        alert('Removing item');
+        event.target.parentNode.parentNode.remove();
+        //newItem.remove();
+    })
+}
+
+// })
+//all remove bttns to remove div on click
+
+let instockOrOutstock = true
+
+const allInstockButtons = document.querySelectorAll('#stock')
+
+for (const stockBttn of allInstockButtons) {
+    stockBttn.addEventListener('click', () => {
+
+        instockOrOutstock = !instockOrOutstock
+
+        if (instockOrOutstock) {
+            stockBttn.innerText = "InStock"
+            stockBttn.style.color = 'black'
+        } else {
+            stockBttn.innerText = "Out Of Stock"
+            stockBttn.style.color = 'red'
+        }
+
+    })
+}
+
 
 
 // //provide error message if required fields arent filled 
 
 
 const formSubmit = document.querySelector('.add-BAG');
-//.wrapper
+
 console.log(formSubmit)
 
 formSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    // console.log(formSubmit)
 
     const bagSection = document.querySelector('section');
     // console.log(bagSection)
     const newItem = document.createElement('div');
-    newItem.classList.add('bag-with-text');
+    newItem.classList.add('bag-with-text2');
 
     bagSection.append(newItem);
 
@@ -42,7 +74,7 @@ formSubmit.addEventListener('submit', (event) => {
     // newRmvBttn.addEventListener('click', () => { alert('Removing item'); });
 
     const newRmvBttn = document.createElement('button');
-    newRmvBttn.id = 'rmv';
+    newRmvBttn.id = 'rmv1';
     newRmvBttn.type = 'button';
     newRmvBttn.onclick = "alert('Removing item')";
     newRmvBttn.innerText = 'Remove';
@@ -61,95 +93,143 @@ formSubmit.addEventListener('submit', (event) => {
     formSubmit.reset();
 
 
+    // const rmvItem = document.querySelector('.bag-with-text2')
+    // console.log(rmvItem)
+    // rmvItem.addEventListener('click', (event) => {
+    //     alert('Removing item');
+    //     rmvItem.remove()
 
-    const allRemoveButtons = document.querySelectorAll('#rmv');
 
-    for (const rmvBttn of allRemoveButtons) {
-        rmvBttn.addEventListener('click', (event) => {
-            alert('Removing item');
-            event.target.parentNode.parentNode.remove()
-        
-        })
-        // rmvBttn.addEventListener('click', () => {
-        //        
-        //         
-        //       })
-    }
-    //all remove bttns to remove div on click
-
-    let instockOrOutstock = true
-
-    const allInstockButtons = document.querySelectorAll('#stock')
-
-    for (const stockBttn of allInstockButtons) {
-        stockBttn.addEventListener('click', (event) => {
-            instockOrOutstock = !instockOrOutstock
-
-            if (instockOrOutstock) {
-                stockBttn.innerText = "InStock"
-                stockBttn.style.color = 'black'
-            } else {
-                stockBttn.innerText = "Out Of Stock"
-                stockBttn.style.color = 'red'
-            }
-
-        })
-    }
 })
-// ///add functionality to color changing buttons
 
-// ""
 
-// const formSubmit = document.querySelector('.add-BAG');
-// console.log("initial", formSubmit);
 
-// formSubmit.addEventListener('submit', (event) => {
-//   event.preventDefault();
+const rmvItem = document.querySelector('.bag-with-text2')
+console.log(rmvItem)
+rmvItem.addEventListener('click', (event) => {
+    alert('Removing item');
+    rmvItem.remove()
+})
+    // newRmvBttn.addEventListener('click', () => { alert('Removing item'); });
 
-//   const bagSection = document.querySelector('section');
-//   const newItem = document.createElement('div');
-//   newItem.classList.add('bag-with-text');
+//     const allRemoveButtons = document.querySelectorAll('#rmv');
+//     allRemoveButtons.addEventListener('click', (event) => {
+//         for (const rmvBttn of allRemoveButtons) {
+//             if (rmvBttn) {
+//                 alert('Removing item');
+//                 event.target.parentNode.parentNode.remove();
+//                 // newItem.remove();
+//             }
+//         }
 
-//   const bagImage = document.createElement('img');
-//   bagImage.src = document.querySelector('#form3').value;
-//   bagImage.alt = 'default image';
+//     })
+//     //all remove bttns to remove div on click
 
-//   const bagName = document.createElement('h2');
-//   bagName.innerText = document.querySelector('#form1').value;
+//     let instockOrOutstock = true
 
-//   const newForm = document.createElement('form');
-//   newForm.action = 'bag_inventory2';
-//   newForm.method = 'post';
+//     const allInstockButtons = document.querySelectorAll('#stock')
 
-//   const newStckBttn = document.createElement('button');
-//   newStckBttn.id = 'stock';
-//   newStckBttn.type = 'button';
-//   newStckBttn.innerText = 'Instock';
+//     for (const stockBttn of allInstockButtons) {
+//         stockBttn.addEventListener('click', () => {
 
-//   const newRmvBttn = document.createElement('button');
-//   newRmvBttn.id = 'rmv';
-//   newRmvBttn.type = 'button';
-//   newRmvBttn.innerText = 'Remove';
+//             instockOrOutstock = !instockOrOutstock
 
-//   newForm.append(newStckBttn);
-//   newForm.append(newRmvBttn);
+//             if (instockOrOutstock) {
+//                 stockBttn.innerText = "InStock"
+//                 stockBttn.style.color = 'black'
+//             } else {
+//                 stockBttn.innerText = "Out Of Stock"
+//                 stockBttn.style.color = 'red'
+//             }
 
-//   newItem.append(bagImage);
-//   newItem.append(bagName);
-//   newItem.append(newForm);
+//         })
+//     }
+// })
 
-//   bagSection.append(newItem);
 
-//   formSubmit.reset();
+// // ///add functionality to color changing buttons
 
-//   console.log("after reset", formSubmit);
-//   newRmvBttn.addEventListener('click', () => {
-//     alert('Removing item');
-//     newItem.remove();
-//   })
-//   newStckBttn.addEventListener('click', () => {
-//     newStckBttn.innerText = newStckBttn.innerText === 'Instock' ? 'Out Of Stock' : 'Instock';
-//     newStckBttn.style.color = newStckBttn.innerText === 'Instock' ? 'black' : 'red';
-//   });
-//   console.log("end", formSubmit);
-// });
+// // ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const formSubmit = document.querySelector('.add-BAG');
+// // console.log("initial", formSubmit);
+
+// // formSubmit.addEventListener('submit', (event) => {
+// //   event.preventDefault();
+
+// //   const bagSection = document.querySelector('section');
+// //   const newItem = document.createElement('div');
+// //   newItem.classList.add('bag-with-text');
+
+// //   const bagImage = document.createElement('img');
+// //   bagImage.src = document.querySelector('#form3').value;
+// //   bagImage.alt = 'default image';
+
+// //   const bagName = document.createElement('h2');
+// //   bagName.innerText = document.querySelector('#form1').value;
+
+// //   const newForm = document.createElement('form');
+// //   newForm.action = 'bag_inventory2';
+// //   newForm.method = 'post';
+
+// //   const newStckBttn = document.createElement('button');
+// //   newStckBttn.id = 'stock';
+// //   newStckBttn.type = 'button';
+// //   newStckBttn.innerText = 'Instock';
+
+// //   const newRmvBttn = document.createElement('button');
+// //   newRmvBttn.id = 'rmv';
+// //   newRmvBttn.type = 'button';
+// //   newRmvBttn.innerText = 'Remove';
+
+// //   newForm.append(newStckBttn);
+// //   newForm.append(newRmvBttn);
+
+// //   newItem.append(bagImage);
+// //   newItem.append(bagName);
+// //   newItem.append(newForm);
+
+// //   bagSection.append(newItem);
+
+// //   formSubmit.reset();
+
+// //   console.log("after reset", formSubmit);
+// //   newRmvBttn.addEventListener('click', () => {
+// //     alert('Removing item');
+// //     newItem.remove();
+// //   })
+// //   newStckBttn.addEventListener('click', () => {
+// //     newStckBttn.innerText = newStckBttn.innerText === 'Instock' ? 'Out Of Stock' : 'Instock';
+// //     newStckBttn.style.color = newStckBttn.innerText === 'Instock' ? 'black' : 'red';
+// //   });
+// //   console.log("end", formSubmit);
+// // });
+
